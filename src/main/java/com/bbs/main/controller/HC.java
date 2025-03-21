8bc12e29b3783477a69ce5de7143f277cc27a014
wonho <dutchface498@gmail.com>
2025-03-21 10:55:57 +0900

    <wh> thirteenth commit

--- src/main/java/com/bbs/main/controller/HC.java
+++ src/main/java/com/bbs/main/controller/HC.java
@@ -25,7 +25,9 @@ public class HC {
     @GetMapping("/")
     public String home(Model model) {
         model.addAttribute("free", freeService.getposts());
+        model.addAttribute("life", lifeService.getposts());
         model.addAttribute("tour", tourService.getAllLocation("1", null, "R"));
+
         model.addAttribute("content", "wh/main.jsp");
         return "index";
     }
